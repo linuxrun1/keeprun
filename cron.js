f6b3199f3f7da73094b70f85d50cb1319ec0f9ad
hi <hi@hi.local>
2022-11-16 22:55:27 +0700

    up

--- cron.js
+++ cron.js
@@ -1,5 +1,5 @@
 var cron = require('node-cron');
 const curl = require('./lib/curl');
-cron.schedule('10 * * * *', () => {
+cron.schedule('* * * * *', () => {
   curl('https://keeprun.myfam2.repl.co');
 });
